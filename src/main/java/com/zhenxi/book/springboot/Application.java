c1857769cc3b97e422a9a5499b8f826d2885141d
ZhenxiKIm <8055kjh@gmail.com>
2021-03-11 20:32:49 +0900

    8.4 스프링 부트 프로젝트로 RDS로 접근하기  - RDS 환경 profile 설정 추가

--- src/main/java/com/zhenxi/book/springboot/Application.java
+++ src/main/java/com/zhenxi/book/springboot/Application.java
@@ -2,11 +2,6 @@ package com.zhenxi.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 @SpringBootApplication
 public class Application {
